a44c2118a06feb20850bbfd6724f924173d10880
aksharakhare <aksharaiskhare@gmail.com>
2022-04-25 20:13:33 -0400

    Database working

--- components/Profile/index.js
+++ components/Profile/index.js
@@ -13,11 +13,37 @@ import {
   ActivityIndicator,TouchableOpacity
 } from 'react-native';
 const Profile=({navigation})=>{
-
-      
+    const[image,setImage]=useState('');
+    const[name,setName]=useState("");
+    db.collection("Userdata").doc(auth.currentUser.uid).get('user_prof').then((doc) => {  //Note the return here
+      if (doc.exists) {
+              setImage(doc.data().user_prof);
+              setName(doc.data().name);  //Note the return here
+          } else {
+              console.log("No such document!");
+              //Handle this situation the way you want! E.g. return false or throw an error
+              return false;
+          }
+      }).catch(error => {
+          console.log("Error getting document:", error);
+          //Handle this situation the way you want
+      });
 return(
     <View style={styles.container}>
-        <Text>Profile</Text>
+        <View style={{}}><Text style={{
+    position:'relative',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    textAlign:'left',
+    fontSize: 25,
+    lineHeight: 58,
+    color: '#5C514D',
+    marginTop: '30%',
+    marginRight:'3%',
+    marginLeft: '5%',
+  }}>Hi {name} </Text>
+        <Image source={{uri:image}} style={{width:'40%',height:'35%',borderRadius:90,marginLeft:'60%',marginTop:'-22%'}}/>
+        </View>
         <Button title='Click here' onPress={console.log("hi")}/>
       </View>
     );
@@ -26,8 +52,8 @@ export default Profile;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop:'1%',
+    marginHorizontal:'6%',
     backgroundColor: '#F3F0EC'
   },
   
